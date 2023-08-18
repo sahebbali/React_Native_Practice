aaceb30c9ebf943a227f4e96bc3fec3796d960d1
sahebbali <sahebbali253@gmail.comm>
2023-08-18 07:04:33 +0600

    add flatList component

--- App.tsx
+++ App.tsx
@@ -2,26 +2,13 @@ import React, { Component } from "react";
 import { View, Text,Button } from "react-native";
 import {useState} from 'react'
 import Component1 from './components/component1.js'
+import FloatList from "./components/FloatList.js";
 
 const App= ()=>{
-const [state, setState]=useState({
-    name: "Sahib",
-    age: 30,
-    city: "Dhaka"
-})
-
-const onChange =()=>{
-    setState({name:"Sajid ALi", age: 14, city:"Dinajpur"});
-}
 
   return(
         <View>
-          <Text>This is our First Component</Text>
-          <Text>Name: {state.name}</Text>
-          <Text>Age: {state.age}</Text>
-          <Text>City: {state.city}</Text>
-          <Button onPress={onChange} title="CLick Me"/>
-          <Component1 name="Ali Khan" age="34" city="Rajshahi"/>
+          <FloatList />
         </View>
   )
 }
--- components/FloatList.js
+++ components/FloatList.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
 
-export default class FloatList extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
+const data = [
+  { id: '1', title: 'Bangladesh is small' },
+  { id: '2', title: 'Dhaka is old' },
+  { id: '3', title: 'Dinajpur is a new' },
+   { id: '4', title: 'Bangladesh is small' },
+  { id: '5', title: 'Dhaka is old' },
+  { id: '6', title: 'Dinajpur is a new' },
+  // ... more data ...
+];
+
+const MyFlatList = () => {
+  const renderItem = ({ item }) => (
+    <View>
+      <Text style={styles.titel}>{item.title}</Text>
+    </View>
+  );
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+};
+
+export default MyFlatList;
+
+const styles= StyleSheet.create({
+  titel:{
+    color:"gray",
+    fontSize:20,
+    padding:10
+
   }
-}
-
-const styles = StyleSheet.create({})
+})
