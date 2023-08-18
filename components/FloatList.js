09584b4168d8bd550fa1895b31293d400f640d12
sahebbali <sahebbali253@gmail.comm>
2023-08-18 08:43:19 +0600

    add flatlist functionality

--- components/FloatList.js
+++ components/FloatList.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { View, FlatList, Text, Image, StyleSheet, Alert, TouchableOpacity } from 'react-native';
 
 const data = [
-  { id: '1', title: 'Bangladesh is small' },
-  { id: '2', title: 'Dhaka is old' },
-  { id: '3', title: 'Dinajpur is a new' },
-   { id: '4', title: 'Bangladesh is small' },
-  { id: '5', title: 'Dhaka is old' },
-  { id: '6', title: 'Dinajpur is a new' },
+  { id: '1', title: 'Bangladesh', subTitle:"Dhaka", imageUrl: 'https://media.istockphoto.com/id/1366452855/vector/bangladesh-national-flag-vektor-illustration.jpg?s=1024x1024&w=is&k=20&c=znJOYPOr4Ravx2gCbgDMvXTZtcGB1JHyISsWaems_Y0=' },
+  { id: '2', title: 'India', subTitle:"Dilliy", imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/800px-Flag_of_India.svg.png' },
+  { id: '3', title: 'Pakisthan',subTitle:"Islamabad", imageUrl: 'https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg' },
   // ... more data ...
 ];
 
-const MyFlatList = () => {
+onClick=(alertTitel)=>{
+  Alert(alertTitel )
+}
+const MyFlatListWithImages = () => {
+
+   const handleItemClick = (title) => {
+   Alert.alert('Clicked Item', `You clicked on item: ${title}`);
+    
+    // Perform additional actions based on the clicked item
+  };
   const renderItem = ({ item }) => (
-    <View>
-      <Text style={styles.titel}>{item.title}</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.itemContainer}
+      onPress={() => handleItemClick(item.title)}
+    >
+      <Image source={{ uri: item.imageUrl }} style={styles.image} />
+      <View style={styles.title}>
+        <Text style={styles.headerTitle}>{item.title}</Text>
+      <Text>{item.subTitle}</Text>
+      </View>
+    </TouchableOpacity>
   );
 
   return (
@@ -27,13 +40,30 @@ const MyFlatList = () => {
   );
 };
 
-export default MyFlatList;
-
-const styles= StyleSheet.create({
-  titel:{
-    color:"gray",
-    fontSize:20,
-    padding:10
+const styles = StyleSheet.create({
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+    borderRadius: 25,
+  },
+  title:{
+    display:"flex",
+    flexDirection:"column"
+  },
+  headerTitle:{
+    color:"black",
+    fontSize: 30,
+    padding:3
 
   }
-})
+});
+
+export default MyFlatListWithImages;
